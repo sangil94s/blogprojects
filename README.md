ebc726158abc0f17ace0f0b12666d85b43108bf9
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2025-08-02 15:37:04 +0900

    Add : Husky 추가

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Blog Project
 
 ### 참고 링크
+
 [기획 초안 바로가기](https://precious-hyssop-b3b.notion.site/BlogProject-24150042c170809a8ac0cfe620c73378?pvs=74)
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full px-6">
       <h1>1</h1>
       <Button>Click me</Button>
-
     </div>
   );
 }
